8e0c344760a15da694af57ae93e442c44a298646
Minhazul Ashim <ashim.ashim7470@gmail.com>
2024-03-27 21:35:13 +0600

    added something more

--- review/forms.py
+++ review/forms.py
@@ -4,11 +4,12 @@ from . import models;
 class ReviewForm(forms.ModelForm) :
     class Meta :
         model = models.Review;
-        fields = "__all__"
+        fields = ['name', 'review']
         widgets = {
             "review": forms.Textarea(attrs={"row": 3})
         }
 
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
-            self.fields['book'] = forms.ModelChoiceField(queryset=models.Booko.objects.all())
+            # self.fields['book'] = forms.ModelChoiceField(queryset=models.Booko.objects.all())
+            # self.account = kwargs.pop('account')
